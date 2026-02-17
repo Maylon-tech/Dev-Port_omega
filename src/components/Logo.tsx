5442cf5715226f547591e926766cdc19a7ebb20d
Nemoto Mailon <minatokiroisen77@gmail.com>
2026-02-17 15:55:58 +0900

    building the navbar with logo.

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,10 +1,16 @@
 import Link from "next/link"
+import { LuCodeXml } from "react-icons/lu"
 
 
 const Logo = () => {
   return (
-    <Link href="/">
-        LOGO
+    <Link href="/" className="flex gap-2 items-center">
+        <span className="w-10 h-10 rounded-full bg-gray-800 grid place-items-center">
+          <LuCodeXml />
+        </span>
+        <h2 className="text-gray-200 font-bold md:text-2xl hidden sm:block">
+          NemotoDev
+        </h2>
     </Link>
   )
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,20 +1,62 @@
+
+import Link from "next/link"
 import Logo from "../Logo"
 
-
+const navLinks = [
+  {
+    url: "#home",
+    label: "Home"
+  },
+  {
+    url: "#services",
+    label: "Services"
+  },
+  {
+    url: "#resume",
+    label: "Resume"
+  },
+  {
+    url: "#projects",
+    label: "Projects"
+  },
+  {
+    url: "#skills",
+    label: "Skills"
+  },
+  {
+    url: "#testimonials",
+    label: "Testimonials"
+  },
+  {
+    url: "#contact",
+    label: "Contact"
+  },
+]
 
 const Navbar = () => {
   return (
     <nav className="fixed z-50 flex items-center justify-between p-4 bg-green-800 text-white w-full h-15">
+      {/* LOGO */}
       <div className="text-3xl font-semibold">
         <Logo />
       </div>
       
-      <ul className="flex gap-6">
-        <li>Home</li>
-        <li>About</li>
-        <li>Projects</li>
-        <li>Skills</li>
-        <li>Contact</li>
+      {/* Nav-links */}
+      <ul className="hidden lg:flex space-x-10">
+        {
+          navLinks.map((link) => {
+            return (
+              <li key={link.url}>
+                <Link
+                  href={link.url}
+                  className="text-gray-200 hover:text-cyan-300 font-medium tansition-colors duration-300"
+                >
+                  {link.label}
+                </Link>
+              </li>
+            )
+          })
+        }
       </ul>
       
       <div className="p-2 rounded-lg bg-blue-600 w-30 text-center">
