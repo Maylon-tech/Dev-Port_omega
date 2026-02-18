61119ee77d4d19a77896e47e6944cefef079dab6
Nemoto Mailon <minatokiroisen77@gmail.com>
2026-02-18 15:42:46 +0900

    build a nav mobile type

---
+++ src/components/Navbar/MobileNav.tsx
@@ -0,0 +1,25 @@
+
+
+
+interface MobileNavProps {
+    navOpen: boolean
+}
+
+
+const MobileNav = ({ navOpen }: MobileNavProps) => {
+    
+
+
+    return (
+        <>
+            {/* InsetBackground */}
+
+
+            {/* Navlinks */}
+
+            
+        </>
+    )
+}
+
+export default MobileNav
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,11 @@
+"use client"
 
+import { useState } from "react"
 import Link from "next/link"
 import Logo from "../Logo"
+import LinkButton from "../LinkButton"
+import { LuDownload } from "react-icons/lu"
+import MobileNav from "./MobileNav"
 
 const navLinks = [
   {
@@ -34,6 +39,10 @@ const navLinks = [
 ]
 
 const Navbar = () => {
+  const [navOpen, setNavOpen] = useState(false)
+
+
+
   return (
     <nav className="fixed z-50 flex items-center justify-between p-4 bg-green-800 text-white w-full h-15">
       {/* LOGO */}
@@ -59,9 +68,22 @@ const Navbar = () => {
         }
       </ul>
       
-      <div className="p-2 rounded-lg bg-blue-600 w-30 text-center">
-        Social Links
+      {/* Link Button */}
+      
+      <div className="hidden lg:block">
+        <LinkButton
+          href="/document/cd.pdf"
+          text="Download CV"
+          download 
+          icon={LuDownload}
+          iconPosition="left"
+        />
       </div>
+
+      <MobileNav
+        navOpen={navOpen}
+      />
+
     </nav>
   )
 }
